0bfaeb61bc1bfe3c468c4e1f36ee764667383843
Infi <infiziert@protonmail.ch>
2023-11-29 13:06:04 +0100

    fix french regex

--- DeathRoll/Logic/Reg.cs
+++ DeathRoll/Logic/Reg.cs
@@ -11,8 +11,8 @@ public static class Reg
     private static readonly Regex DiceRollRegexDE = new(@"^Würfeln! (?:\(1-(?<out>\d+)\) )?(?<roll>\d+)");
     private static readonly Regex RandomRollRegexDE = new(@"^(?<player>[a-zA-Z'-]+.? [a-zA-Z'-]*.?|Du) (?:(?:würfelst|würfelt)|(?:hast|hat) mit dem|würfelt) (?<out>\d+)?(?:eine |-seitigen Würfel eine) (?<roll>\d+)(?:\.| gewürfelt!)");
 
-    private static readonly Regex DiceRollRegexFR = new(@"^Lancer (?:de dé|d'un dé )(?<out>\d+)? ! (?<roll>\d+)");
-    private static readonly Regex RandomRollRegexFR = new(@"^(?:Lancer d'un dé (?<out>\d+) ! )?(?<player>Vous|[a-zA-Z'-]+.? [a-zA-Z'-]*.?) (?:jetez|jette|obtenez un|obtient un) (?:les dés et (?:obtenez|obtient) )?(?<roll>\d+) !");
+    private static readonly Regex DiceRollRegexFR = new(@"^Lancer (?:de dé|d'un dé )(?<out>\d+)?! (?<roll>\d+)");
+    private static readonly Regex RandomRollRegexFR = new(@"^(?:Lancer d'un dé (?<out>\d+)! )?(?<player>Vous|[a-zA-Z'-]+.? [a-zA-Z'-]*.?) (?:jetez|jette|obtenez un|obtient un) (?:les dés et (?:obtenez|obtient) )?(?<roll>\d+)!");
 
     private static readonly Regex DiceRollRegexJP = new(@"^ダイス！ (?:MAX(?<out>\d+)  )?(?<roll>\d+)");
     private static readonly Regex RandomRollRegexJP = new(@"^(?:(?<out>\d+)面)?ダイス！ (?<player>[a-zA-Z'-]+.? [a-zA-Z'-]*.?)は、(?<roll>\d+)を出した！");
